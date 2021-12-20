243d3ed57c836b4e9de8780038eea2ee43fc273e
mlNothing <utmostfires@gmail.com>
2021-12-20 16:12:03 +0800

    初始化及登录功能

---
+++ vue.config.js
@@ -0,0 +1,21 @@
+let proxyObj = {}
+proxyObj['/'] = {
+    //websocket
+    ws: false,
+    //目标地址
+    target: 'http://localhost:8081',
+    //发送请求头中host会设置成target
+    changeOrigin: true,
+    pathRewrite: {
+        '^/': '/'
+   }
+}
+    module.exports = {
+        devServer: {
+            host: 'localhost',
+            port: 8080,
+            proxy: proxyObj
+    }
+    }
+
+// 配置请求转发解决跨域问题
