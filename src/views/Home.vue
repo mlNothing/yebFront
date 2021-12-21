344a6a3a67a5a44369e817f998c6119bd03c9814
mlNothing <utmostfires@gmail.com>
2021-12-21 14:13:05 +0800

    添加导航页

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,43 @@
 <template>
     <div>
-       Home
+        <el-container>
+            <el-header class="homeHeader">
+                <div class="title">mlNothing系统</div>
+            </el-header>
+            <el-container>
+                <el-aside width="200px">
+
+                    <!-- select :菜单激活回调 index :选中菜单项的index  indexPath:选中菜单项以及所属父菜单项的index 
+                    渲染路由 router:是否使用vue-router的模式，启用改模式会在激活导航时以index作为path进行路由跳转 我们可以取消之前的select事件
+                    -->
+                    <el-menu router >
+                        <el-submenu index="1" v-for="(item,index) in this.$router.options.routes"  v-if="!item.hidden"  :key="index"  >
+                            <template slot="title">
+                                <i class="el-icon-location"></i>
+                                <span>{{item.name}}</span>
+                            </template>
+                            <el-menu-item :index="children.path" v-for="(children,indexj) in item.children" :key="indexj">{{children.name}}</el-menu-item>
+                        </el-submenu>
+                    </el-menu>
+                </el-aside>
+                <el-main>
+                    <router-view/>
+                </el-main>
+            </el-container>
+        </el-container>
     </div>
 </template>
+
 <script>
     export default {
-        name: "Home"
+        name: 'Home',
+        methods: {
+            menuClick(index) {
+                this.$router.push(index);
+           }
+       }
    }
 </script>
-<style scoped>
+<style>
+    
+</style>
---
+++ src/views/Test1.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+       Test1
+    </div>
+</template>
+<script>
+    export default {
+        name: "Test1"
+   }
+</script>
+<style scoped>
+</style>
