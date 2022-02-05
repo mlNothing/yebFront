fed13c4a2685474bbb59ca991991b94baa0c55de
mlNothing <utmostfires@gmail.com>
2022-02-05 12:57:43 +0800

    高级资料

---
+++ src/views/emp/EmpAdv.vue
@@ -0,0 +1,18 @@
+<template>
+ <div>
+   高级资料
+ </div>
+</template>
+
+<script>
+export default {
+name: "EmpAdv",
+  data () {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+</style>
