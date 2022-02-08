d85bcf9a7cd04c5365f1a978d41beaa73e5a49bb
mlNothing <utmostfires@gmail.com>
2022-02-08 13:39:53 +0800

    修改小问题

---
+++ src/views/sal/SalTable.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    工资表管理
+  </div>
+</template>
+<script>
+export default {
+  name: "SalTable",
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+</style>
